f0d77f0fcf4d19cc2bb5501a4aba6015a5d6e9ab
Thashitharan27 <95272053+Thashitharan27@users.noreply.github.com>
2026-08-21 18:47:37 +0530

    Export core directional feature provider

--- crypto_strategy_lab/features/__init__.py
+++ crypto_strategy_lab/features/__init__.py
@@ -2,5 +2,19 @@
 
 from .base import FeatureDefinition, FeatureProvider
 from .registry import FeatureRegistry
+from .technical import (
+    CORE_DIRECTIONAL_FEATURE_NAME,
+    CORE_DIRECTIONAL_FEATURE_VERSION,
+    CoreDirectionalFeatureProvider,
+    prepare_core_directional_features,
+)
 
-__all__ = ["FeatureDefinition", "FeatureProvider", "FeatureRegistry"]
+__all__ = [
+    "FeatureDefinition",
+    "FeatureProvider",
+    "FeatureRegistry",
+    "CORE_DIRECTIONAL_FEATURE_NAME",
+    "CORE_DIRECTIONAL_FEATURE_VERSION",
+    "CoreDirectionalFeatureProvider",
+    "prepare_core_directional_features",
+]
