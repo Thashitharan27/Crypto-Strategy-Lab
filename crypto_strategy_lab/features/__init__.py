fb45715ba4022267f32d1979ca8833cd0bbee76a
Thashitharan27 <95272053+Thashitharan27@users.noreply.github.com>
2026-08-21 18:58:24 +0530

    Export cached market context features

--- crypto_strategy_lab/features/__init__.py
+++ crypto_strategy_lab/features/__init__.py
@@ -1,6 +1,12 @@
 """Versioned feature-store contracts."""
 
 from .base import FeatureDefinition, FeatureProvider
+from .cache import FeatureFrameCache
+from .context import (
+    MARKET_CONTEXT_FEATURE_NAME,
+    MARKET_CONTEXT_FEATURE_VERSION,
+    MarketContextFeatureProvider,
+)
 from .registry import FeatureRegistry
 from .technical import (
     CORE_DIRECTIONAL_FEATURE_NAME,
@@ -13,8 +19,12 @@ __all__ = [
     "FeatureDefinition",
     "FeatureProvider",
     "FeatureRegistry",
+    "FeatureFrameCache",
     "CORE_DIRECTIONAL_FEATURE_NAME",
     "CORE_DIRECTIONAL_FEATURE_VERSION",
     "CoreDirectionalFeatureProvider",
     "prepare_core_directional_features",
+    "MARKET_CONTEXT_FEATURE_NAME",
+    "MARKET_CONTEXT_FEATURE_VERSION",
+    "MarketContextFeatureProvider",
 ]
