9cacfc08915ccce2089849a97771ccb96a56927b
Thashitharan27 <95272053+Thashitharan27@users.noreply.github.com>
2026-09-19 13:52:06 +0530

    Bump S/R feature version for new zone geometry

--- crypto_strategy_lab/features/support_resistance.py
+++ crypto_strategy_lab/features/support_resistance.py
@@ -26,7 +26,7 @@ from .technical import CORE_DIRECTIONAL_FEATURE_NAME
 
 
 SUPPORT_RESISTANCE_FEATURE_NAME = "support_resistance"
-SUPPORT_RESISTANCE_FEATURE_VERSION = "3"
+SUPPORT_RESISTANCE_FEATURE_VERSION = "4"
 
 
 def _optional_float(value):
